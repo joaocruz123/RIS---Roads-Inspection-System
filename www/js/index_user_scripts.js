72818cf49180a90a32af62167373e95522f3fce4
João Paulo Franco da Cruz <joaofranco.cruz@gmail.com>
2017-02-26 22:08:02 -0300

    Tela principal do Usuaria, Icons de acesso e barra footer de navegação

--- www/js/index_user_scripts.js
+++ www/js/index_user_scripts.js
@@ -33,6 +33,56 @@
          $(".uib_w_11").modal("toggle");  
     });
     
+        /* button  .uib_w_9 */
+    
+    
+        /* button  .uib_w_9 */
+    
+    
+        /* button  .uib_w_31 */
+    $(document).on("click", ".uib_w_31", function(evt)
+    {
+         /*global activate_subpage */
+         activate_subpage("#page_87_79"); 
+    });
+    
+        /* button  .uib_w_14 */
+    
+    
+        /* button  .uib_w_14 */
+    $(document).on("click", ".uib_w_14", function(evt)
+    {
+        /* your code goes here */ 
+    });
+    
+        /* button  .uib_w_14 */
+    $(document).on("click", ".uib_w_14", function(evt)
+    {
+         /*global activate_page */
+         activate_page("#mainpage"); 
+    });
+    
+        /* button  .uib_w_9 */
+    $(document).on("click", ".uib_w_9", function(evt)
+    {
+         /*global activate_page */
+         activate_page("#painel_user"); 
+    });
+    
+        /* button  .uib_w_30 */
+    $(document).on("click", ".uib_w_30", function(evt)
+    {
+         /*global activate_subpage */
+         activate_subpage("#sobre"); 
+    });
+    
+        /* button  .uib_w_42 */
+    $(document).on("click", ".uib_w_42", function(evt)
+    {
+         /*global activate_page */
+         activate_page("#mainpage"); 
+    });
+    
     }
  document.addEventListener("app.Ready", register_event_handlers, false);
 })();
